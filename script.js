72f598c37359be00e4982303248113d88ca190d4
jamal <jamal.rzayev@gmail.com>
2022-05-02 17:49:18 +0400

    board_is_ready

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// Display/UI
 
+import { createBoard } from "./minesweeper.js";
 
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 2;
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector('.board');
+
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element);
+    });
+});
+boardElement.style.setProperty("--size", BOARD_SIZE);
 
 // 1. Populate a board with tiles/mines
 // 2. Left click on tiles
